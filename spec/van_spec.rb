8c0ee06e036b7351b1156fb785f20e08addf96dd
Vish Mayer <vish.mayer@gmail.com>
2020-04-17 15:07:58 +0100

    improved testing for van and general cleanup

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -3,11 +3,11 @@ require 'van'
 describe Van do
 
   subject(:van) { described_class.new }
-  let(:garage) {double(:bikes => [working_bike, working_bike])}
-  let(:bike) {double(:broken? => true)}
-  let(:working_bike) {double(:working? => true)}
-  let(:station) {double(:bikes => [bike])}
-
+  let(:garage) {double(:bikes => [broken_bike, working_bike])}
+  let(:station) {double(:bikes => [broken_bike, working_bike])}
+  let(:broken_bike) {double(:broken? => true, :working? => false)}
+  let(:working_bike) {double(:working? => true, :broken? => false)}
+  
   it { is_expected.to respond_to(:broken_bikes) }
   it { is_expected.to respond_to(:fixed_bikes) }
 
@@ -23,23 +23,23 @@ describe Van do
   it { is_expected.to respond_to(:pick_up_broken).with(1).argument }
 
   describe '#pick_up_broken' do
-    it 'picks up broken bikes from a dockingstation' do
+    it 'only picks up broken bikes from a dockingstation' do
       van.pick_up_broken(station)
-      expect(van.broken_bikes).to eq [bike]
+      expect(van.broken_bikes).to eq [broken_bike]
     end
-    it 'removes broken bike from a dockingstation' do
+    it 'removes broken bikes from a dockingstation' do
       van.pick_up_broken(station)
-      expect(station.bikes).to_not eq [bike]
+      expect(station.bikes).to eq [working_bike]
     end
   end
 
   it { is_expected.to respond_to(:deliver_broken).with(1).argument }
 
   describe '#deliver_broken' do
-    it 'drops of bikes to a garage to be fixed' do
+    it 'drops of broken bikes to a garage to be fixed' do
       van.pick_up_broken(station)
       van.deliver_broken(garage)
-      expect(garage.bikes).to eq [working_bike, working_bike, bike]
+      expect(garage.bikes).to eq [broken_bike, working_bike, broken_bike]
     end
     it 'removes broken bikes from a van ' do
       van.pick_up_broken(station)
@@ -51,13 +51,13 @@ describe Van do
   it { is_expected.to respond_to(:pick_up_fixed).with(1).argument }
 
   describe '#pick_up_fixed' do
-    it 'picks up all working bikes from a garage' do
+    it 'only picks up working bikes from a garage' do
       van.pick_up_fixed(garage)
-      expect(van.fixed_bikes).to eq [working_bike, working_bike]
+      expect(van.fixed_bikes).to eq [working_bike]
     end
     it 'removes all working bikes from a garage' do
       van.pick_up_fixed(garage)
-      expect(garage.bikes).to_not eq [working_bike, working_bike]
+      expect(garage.bikes).to eq [broken_bike]
     end
   end
 
@@ -67,7 +67,7 @@ describe Van do
     it 'drops of working bikes to a station' do
       van.pick_up_fixed(garage)
       van.deliver_fixed(station)
-      expect(station.bikes).to eq [bike, working_bike, working_bike]
+      expect(station.bikes).to eq [broken_bike, working_bike, working_bike]
     end
     it 'removes working bikes from a van' do
       van.pick_up_fixed(garage)
