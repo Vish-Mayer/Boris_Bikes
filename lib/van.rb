54a31cceb8a4fb4302b59dc39a76e2fa01d155f1
Vish Mayer <vish.mayer@gmail.com>
2020-04-17 13:43:34 +0100

    Final user story complete

--- lib/van.rb
+++ lib/van.rb
@@ -1,9 +1,10 @@
 class Van
 
-  attr_reader :broken_bikes
+  attr_reader :broken_bikes, :fixed_bikes
 
   def initialize
     @broken_bikes = []
+    @fixed_bikes = [] 
   end
 
   def pick_up_broken(station)
@@ -15,4 +16,14 @@ class Van
     @broken_bikes.map { |bike| garage.bikes << bike }
     @broken_bikes.clear
   end
+
+  def pick_up_fixed(garage)
+    garage.bikes.map { |bike| @fixed_bikes << bike if bike.working? }
+    garage.bikes.map { |bike| garage.bikes.delete(bike) if bike.working? }
+  end
+
+  def deliver_fixed(station)
+    @fixed_bikes.map { |bike| station.bikes << bike }
+    @broken_bikes.clear
+  end 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -3,19 +3,26 @@ require 'van'
 describe Van do
 
   subject(:van) { described_class.new }
-  let(:garage) {double(:bikes => [])}
+  let(:garage) {double(:bikes => [working_bike])}
   let(:bike) {double(:broken? => true)}
+  let(:working_bike) {double(:working? => true)}
   let(:station) {double(:bikes => [bike])}
+
+  it { is_expected.to respond_to(:broken_bikes) }
+  it { is_expected.to respond_to(:fixed_bikes) }
 
   describe '#initialize' do
     it 'is initialized with an empty array to store broken bikes' do
       expect(van.broken_bikes).to be_empty
     end
+    it 'is initialized with an empty array to store fixed bikes' do
+      expect(van.fixed_bikes).to be_empty
+    end 
   end
 
   it { is_expected.to respond_to(:pick_up_broken).with(1).argument }
 
-  describe '#pick_up' do
+  describe '#pick_up_broken' do
     it 'picks up broken bikes from a dockingstation' do
       van.pick_up_broken(station)
       expect(van.broken_bikes).to eq [bike]
@@ -26,16 +33,46 @@ describe Van do
     end
   end
 
-  describe '#deliver' do
+  it { is_expected.to respond_to(:deliver_broken).with(1).argument }
+
+  describe '#deliver_broken' do
     it 'drops of bikes to a garage to be fixed' do
       van.pick_up_broken(station)
       van.deliver_broken(garage)
-      expect(garage.bikes).to eq [bike]
+      expect(garage.bikes).to eq [working_bike, bike]
     end
-    it 'drops of bikes to a garage to be fixed' do
+    it 'removes broken bikes from a van ' do
       van.pick_up_broken(station)
       van.deliver_broken(garage)
       expect(van.broken_bikes).to be_empty
     end
   end
+
+  it { is_expected.to respond_to(:pick_up_fixed).with(1).argument }
+
+  describe '#pick_up_fixed' do
+    it 'picks up working bikes from a garage' do
+      van.pick_up_fixed(garage)
+      expect(van.fixed_bikes).to eq [working_bike]
+    end
+    it 'removes working bikes from a garage' do
+      van.pick_up_fixed(garage)
+      expect(garage.bikes).to_not eq [working_bike]
+    end
+  end
+
+  it { is_expected.to respond_to(:deliver_fixed).with(1).argument }
+
+  describe '#deliver_fixed' do
+    it 'drops of working bikes to a station' do
+      van.pick_up_fixed(garage)
+      van.deliver_fixed(station)
+      expect(station.bikes).to eq [bike, working_bike]
+    end
+    it 'removes working bikes from a van' do
+      van.pick_up_fixed(garage)
+      van.deliver_fixed(station)
+      expect(van.broken_bikes).to be_empty
+    end
+  end 
 end
